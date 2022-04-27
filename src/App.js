db3e8053b494cec071a9ebaa68ab75d404dc5e45
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-04-27 13:14:21 +0200

    intial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>UI/UX Design Services</h1>
+        <h2>Mobile App Design</h2>
+        <p>With a strong focus on modern platform </p>
+        guidelines and user needs,
+        <p>Andersen creates unique mobile app designs</p>
       </header>
     </div>
   );
