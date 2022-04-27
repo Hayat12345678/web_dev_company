8a2157a3d8bd637ba346d15e29b1690adfb9aa15
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-04-27 21:35:41 +0200

    initial commit

---
+++ mongodb_backend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  age: Number,
+});
+
+const User = mongoose.model(("User", userSchema));
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await User.find().exec();
+    return req.json(users);
+  } catch (error) {
+    return res.json({ err: err.mesage });
+  }
+  res.json({ meg: "hallo" });
+});
+
+mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+});
