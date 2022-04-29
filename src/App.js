d199cdb58a62686de62d01bda8f54c012a81b081
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-04-29 20:34:47 +0200

    edited db

--- src/App.js
+++ src/App.js
@@ -51,17 +51,21 @@ function App() {
         </div>
         <table>
           <thead>
+            <th>User Id</th>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Age</th>
+            <th>Email Addresse</th>
           </thead>
           <tbody>
             {users.map((user) => {
               return (
                 <tr>
+                  <td>{user._id}</td>
                   <td>{user.firstName}</td>
                   <td>{user.lastName}</td>
                   <td>{user.age}</td>
+                  <td>{user.email}</td>
                 </tr>
               );
             })}
